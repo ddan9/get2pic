29132842e0216d080417fa501fd664a8c90e9fe4
Danil Rednine <r3ddan9@rambler.ru>
2023-05-21 18:11:01 +0300

    Add files via upload

---
+++ get2pic.php
@@ -0,0 +1,53 @@
+<?php
+
+	/////////
+	// GET //
+	/////////
+
+	$image = $_GET["image"];
+
+	$image_decoded = urldecode($image);
+
+	////////////
+	// UPLOAD //
+	////////////
+
+	$filetype = "image/svg+xml";
+
+	$fileraw = base64_encode($image_decoded);
+
+	$filename = hash('md5', time());
+
+	$filesize = strlen($image_decoded);
+
+	$filebody = "data:" . $filetype . ";base64," . $fileraw;
+
+	//////////////
+	// DOWNLOAD //
+	//////////////
+
+	header("Content-Description: File Transfer");
+
+	header("Content-Type: $filetype");
+
+	header("Content-Disposition: attachment; filename=$filename");
+
+	header("Expires: 0");
+
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+
+	header("Pragma: public");
+
+	header("Content-Length: $filesize");
+
+	header("Content-Transfer-Encoding: binary");
+
+	readfile($filebody);
+
+	////////////
+	// ENDING //
+	////////////
+
+	exit();
+
+?>
